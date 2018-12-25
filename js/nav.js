5e6122dd4c713e5010c0bd9ec502227af86a59f4
aryawirasandi <aryawirasandi7153@gmail.com>
2018-12-25 19:27:12 +0700

    AddEventListener for XMLHttpRequest

--- js/nav.js
+++ js/nav.js
@@ -1,52 +1,59 @@
 document.addEventListener("DOMContentLoaded", function(){
-    // Activate sidebar nav
-    var elems = document.querySelectorAll('.sidenav');
+    // active sidenav
+    var elems = document.querySelectorAll(".sidenav");
     M.Sidenav.init(elems);
     loadNav();
-
-    function loadNav()
-    {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange == function ()
-        {
-            if(this.readState == 4)
-            {
-                if(this.status != 200) return;
-
-                //muat daftar tautan menu
-                document.querySelectorAll(".topnav", ".sidenav").forEach(function(elm){
-                    elm.innerHTML = xhttp.responseText;
-                });
-            }
-        }
-        xhttp.open('GET', "nav.html", true);
-        xhttp.send();
-    }
-
+    // Load Page Content
     var page = window.location.hash.substr(1);
     if (page == "") page = "home";
     loadPage(page);
 
-    function loadPage(page)
+    function loadNav(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if(this.readyState == 4)
+            {
+                if(this.status != 200) return;
+                // Muat Daftar Tautan Menu
+                document.querySelectorAll(".topnav, .sidenav").forEach(function(elm){
+                    elm.innerHTML = xhttp.responseText;
+                });
+                // Daftarkan event listener untuk setiap tautan baru
+                document.querySelectorAll(".sidenav a, .topnav a").forEach(function(elm){
+                    elm.addEventListener("click", function (event){
+                        //Tutup Sidenav
+                        var sidenav = document.querySelector(".sidenav");
+                        M.Sidenav.getInstance(sidenav).close();
+                        // Muat konten halaman yang di panggil
+                        page = event.target.getAttribute("href").substr(1);
+                        loadPage(page);
+                    });
+                });
+            }
+        };
+        xhttp.open("GET", "nav.html", true);
+        xhttp.send();
+    }
+
+    function loadPage()
     {
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechang == function ()
-        {
-            if(this.readState == 4)
+        xhttp.onreadystatechange = function (){
+            if(this.readyState == 4)
             {
                 var content = document.querySelector("#body-content");
                 if(this.status == 200)
                 {
                     content.innerHTML = xhttp.responseText;
-                }else if (this.status == 404)
+                }else if(this.status == 404)
                 {
-                    content.innerHTML = "<p>Halaman tidak ditemukan</p>";
+                    content.innerHTML = "<p>Halaman Tidak Ditemukan</p>"
                 }else{
-                    content.innerHTML = "<p>Ups.. halaman tidak dapat diakes. </p>";
+                    content.innerHTML = "<p>Uppss... Halaman Tidak Dapat di akses</p>"
                 }
             }
-        }
-        xhttp.open("GET", "pages/" + page + ".html", true);
+        };
+        xhttp.open("GET", "pages/"+page+".html",true);
         xhttp.send();
     }
 });
